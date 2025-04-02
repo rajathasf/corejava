b76fe7ac95408be7d9688110daf4ecdd877d12bc
rajathasf <rajathsakrex14@gmail.com>
2025-04-02 14:22:17 +0530

    initial commit

---
+++ JavaCode/src/Class1.java
@@ -0,0 +1,14 @@
+public class Class1 {
+    public static void main(String[] args) {
+        int v;
+        v=2;
+        if(v==3){
+            System.out.println("lessgo");
+        }
+        else{
+            System.out.println("no lessgo");
+        }
+
+
+    }
+}
